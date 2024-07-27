911a2e7c75f976f88262eb8086b7509d3b29953c
Akzhan Mataev <150265945+AkiAkzh@users.noreply.github.com>
2024-07-27 22:33:18 +0500

    Add JS task array-diff

--- JavaScript/8kyu/ShortLongShort.js
+++ JavaScript/8kyu/ShortLongShort.js
@@ -1,3 +1,17 @@
+// DESCRIPTION:
+// Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
+
+// Hint for R users:
+
+// The length of string is not always the same as the number of characters
+// For example: (Input1, Input2) --> output
+
+// ("1", "22") --> "1221"
+// ("22", "1") --> "1221"
+// ShortLongShort.solution("1", "22"); // returns "1221"
+// ShortLongShort.solution("22", "1"); // returns "1221"
+
+//Solution
 function solution(a, b) {
     if (a.length < b.length) {
         return a + b + a;
@@ -5,4 +19,4 @@ function solution(a, b) {
         return b + a + b;
     }
 };
-console.log(solution("Aboba", "Boba"));
+console.log(solution("1", "22"));
