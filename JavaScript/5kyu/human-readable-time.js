0d26259e85ddbfdefd42987cb004546bae662018
Akzhan Mataev <150265945+AkiAkzh@users.noreply.github.com>
2024-07-29 16:41:23 +0500

    Add js task human readable time

---
+++ JavaScript/5kyu/human-readable-time.js
@@ -0,0 +1,21 @@
+// DESCRIPTION
+// Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+// HH = hours, padded to 2 digits, range: 00 - 99
+// MM = minutes, padded to 2 digits, range: 00 - 59
+// SS = seconds, padded to 2 digits, range: 00 - 59
+// The maximum time never exceeds 359999 (99:59:59)
+
+// You can find some examples in the test fixtures.
+
+
+//SOLUTION
+
+function humanReadable (seconds) {
+    let result =[];
+    const pad = (num) => num.toString().padStart(2, '0');
+      const hours = Math.floor(seconds/ 3600);
+      const minutes = Math.floor((seconds % 3600) / 60);
+      const secs = seconds % 60;
+      return `${pad(hours)}:${pad(minutes)}:${pad(secs)}`;
+  }
