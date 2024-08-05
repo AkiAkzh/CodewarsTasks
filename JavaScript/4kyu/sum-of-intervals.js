6e0c200138c3e4631f3569a01ca10eb5fe1ac993
Akzhan Mataev <150265945+AkiAkzh@users.noreply.github.com>
2024-08-05 15:18:23 +0500

    Add JS task sum of intervals

---
+++ JavaScript/4kyu/sum-of-intervals.js
@@ -0,0 +1,21 @@
+function sumIntervals(intervals) {
+    if (intervals.length === 0) return 0;
+    intervals.sort((a, b) => a[0] - b[0]);
+    let totalLength = 0;
+    let [currentStart, currentEnd] = intervals[0];
+
+    for (let i = 1; i < intervals.length; i++) {
+        const [start, end] = intervals[i];
+
+        if (start <= currentEnd) {
+            currentEnd = Math.max(currentEnd, end);
+        } else {
+            totalLength += currentEnd - currentStart;
+            currentStart = start;
+            currentEnd = end;
+        }
+    }
+    totalLength += currentEnd - currentStart;
+
+    return totalLength;
+}
